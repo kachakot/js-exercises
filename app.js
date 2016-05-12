a522f995f57cba73278a655ede0b4c9d1414c0bd
Kasia K <k.kotynska91@gmail.com>
2016-05-12 19:09:02 +0200

    Part 2 of JS exercises.

--- app.js
+++ app.js
@@ -49,13 +49,14 @@ for (i = 0; i < 16; i++) {
         console.log(i + " jest nieparzysta");
 }
 
+
 /* JavaScript - Ćwiczenia - Funkcje
    ========================================================================== */
 
-console.log("\n\n JavaScript - Ćwiczenia - Funkcje");
+console.log("\n\n JavaScript - Ćwiczenia - Funkcje\n\n");
 
 /* Ćwiczenie 1 */
-console.log("\n\n ----------------- Cwiczenie 1 -----------------")
+console.log("----------------- Cwiczenie 1 -----------------")
 
 var warzywo;
 
@@ -164,7 +165,6 @@ console.log(rentCost(8));
 /* Ćwiczenie 7 */
 console.log("\n\n ----------------- Cwiczenie 7 -----------------")
 
-
 var rockPaperScissor = function(player1, player2) {
     if (player1 == player2) {
         return "The result is a tie!"
@@ -193,3 +193,163 @@ var rockPaperScissor = function(player1, player2) {
 };
 
 console.log(rockPaperScissor('scissors', 'paper'));
+
+
+/* Ćwiczenie 8 */
+console.log("\n\n ----------------- Cwiczenie 8 -----------------")
+
+var calculateTip = function(amount, raiting) {
+    if (raiting == "Bardzo dobra obsługa")
+        return amount * 0.25;
+    else if (raiting == "Dobra obsługa")
+        return amount * 0.2;
+    else if (raiting == "Średnia obsługa")
+        return amount * 0.15;
+    else if (raiting == "Zła obsługa")
+        return 0;
+    else
+        return "Opis nieczytelny";
+};
+
+console.log(calculateTip(100, "Bardzo dobra obsługa"));
+
+
+/* JavaScript - Ćwiczenia - Tablice
+   ========================================================================== */
+
+console.log("\n\n JavaScript - Ćwiczenia - Tablice \n\n");
+
+/* Ćwiczenie 1 */
+console.log("----------------- Cwiczenie 1 -----------------")
+
+var array = new Array(13, 16, 19, 22, 25);
+
+console.log("zwykła funkcja" + "\n");
+var printTable = function(array) {
+    var i;
+    for (i = 0; i < array.length; i++) {
+        console.log(array[i]);
+    }
+    return;
+};
+
+console.log(printTable(array));
+
+
+console.log("\n" + "metoda forEach()" + "\n");
+array.forEach(function(array) {
+    console.log(array);
+});
+
+
+/* Ćwiczenie 2 */
+console.log("\n\n ----------------- Cwiczenie 2 -----------------")
+
+var array = new Array(1, 2, 3, 4, 5, 6);
+
+var add = function(array) {
+    var i;
+    var suma = 0;
+    for (i = 0; i < array.length; i++) {
+        suma = suma + array[i];
+    }
+    return suma;
+};
+
+var multiply = function(array) {
+    var i;
+    var iloczyn = 1;
+    for (i = 0; i < array.length; i++) {
+        iloczyn = iloczyn * array[i];
+    }
+    return iloczyn;
+};
+
+console.log("wynik dodawania: " + add(array));
+console.log("wynik mnożenia: " + multiply(array));
+
+
+/* Ćwiczenie 3 */
+console.log("\n\n ----------------- Cwiczenie 3 -----------------")
+
+var tab1 = ["abcdefghij", "abc", "abcd efgh ijkl", "abc def", "123456", "123 456 abc", "1234abcdefgh"];
+var tab2 = [];
+var tab3 = [];
+
+tab1.forEach(function(tab1) {
+    if (tab1.length > 9 && tab1.match(/[a-z]/) && tab1.indexOf(' ') < 1) {
+        tab2.push(tab1);
+        tab3.push(true);
+    } else {
+        tab3.push(false);
+    }
+});
+
+console.log(tab1);
+console.log(tab3);
+console.log(tab2);
+
+
+/* Ćwiczenie 4 */
+console.log("\n\n ----------------- Cwiczenie 4 -----------------")
+
+var array = [4, 65, 99, 7, 0, 13, 23];
+
+var reserveSorted = function(array) {
+    return array.sort(function(a, b) {
+        return a - b;
+    }).reverse();
+};
+
+console.log(reserveSorted(array));
+
+
+/* Ćwiczenie 5 */
+console.log("\n\n ----------------- Cwiczenie 5 -----------------")
+
+var number = 3;
+
+var createMultiTable = function(number) {
+    var i;
+    var j;
+    var tab = new Array();
+    var x = 1;
+    for (i = 0; i < number; i++) {
+        tab[i] = new Array();
+        for (j = 0; j < number; j++) {
+            tab[i][j] = x;
+            x++;
+        }
+    }
+    return tab;
+};
+
+var tab = (createMultiTable(number));
+console.log(tab);
+
+tab.forEach(function(tab) {
+    console.log(tab);
+})
+
+
+/* Ćwiczenie 6 */
+console.log("\n\n ----------------- Cwiczenie 6 -----------------")
+console.log("jako parametr przekazano tablicę z ćw. 5" + "\n\n");
+console.log("zwykła funkcja" + "\n");
+var print2DTable = function(tab) {
+    var i;
+    var j;
+    for (i = 0; i < tab.length; i++) {
+        for (j = 0; j < tab.length; j++) {
+            console.log(tab[i][j]);
+        }
+    }
+    return tab;
+};
+
+console.log(print2DTable(tab));
+
+console.log("\n" + "metoda forEach()" + "\n");
+tab.forEach(function(tab) {
+    console.log(tab);
+});
